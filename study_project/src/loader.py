7294886443f767c58608adfed3e2a08e1b86a5a7
Marina Berdnikova <berdni4ka@rambler.ru>
2023-02-12 18:08:24 +0300

    Fix loader.py

--- study_project/src/loader.py
+++ study_project/src/loader.py
@@ -3,8 +3,4 @@ import requests
 
 def load_operations(url):
     response = requests.get(url)
-    #print(response.status_code)
-    #if response.status_code != 200:
-        #return None, f'ERROR: {response.status_code}'
-    #else:
     return response.json()
