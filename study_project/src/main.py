e600bc94f80155c433d9804a09c3da3cbe7e20b3
Marina Berdnikova <berdni4ka@rambler.ru>
2023-02-12 17:51:49 +0300

    Add main.py

--- study_project/src/main.py
+++ study_project/src/main.py
@@ -1,15 +1,15 @@
 import datetime as dt
-import json
 
 from study_project.src import loader
 from study_project.src.formatters import OperationFormatter
 
+URL = 'https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676299637005&signature=3nYMm1hPOoKmqMyKv9THm4RpvhZZLyz8rh50ibDP20M&downloadName=operations.json'
 
-URL = 'https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1676143791543&signature=svPmDVTQ4WmANY7WpsZToVOm23ANOrUaIlunNt2aGdI&downloadName=operations.json'
+
 INITIAL_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
 EXECUTED_OPERATION_STATE = 'EXECUTED'
 
-OPERATION_COUNT = 5
+DISPLAYED_OPERATION_COUNT = 5
 
 RESULT_DATE_FORMAT = '%d.%m.%Y'
 CARD_NUMBER_FORMAT = 'XXXX XX** **** XXXX'
@@ -26,24 +26,18 @@ def convert_date_strings(operations):
 
 
 def get_last_executed_operations(operations):
-    last_executed_operations = []
-    counter = 0
+
     operations_with_date = filter(
-        lambda operation: 'date' in operation, operations
+        lambda operation: (
+                'date' in operation and operation['state'] == EXECUTED_OPERATION_STATE
+        ),
+        operations
     )
-    for operation in sorted(
+    return sorted(
         operations_with_date,
         key=lambda operation: operation['date'],
         reverse=True,
-    ):
-        if operation['state'] == EXECUTED_OPERATION_STATE:
-            last_executed_operations.append(operation)
-            counter += 1
-
-        if counter == OPERATION_COUNT:
-            break
-
-    return last_executed_operations
+    )[:DISPLAYED_OPERATION_COUNT]
 
 
 def main():
