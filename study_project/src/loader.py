64866046cc9291c39efb3da84020b94edcf7779d
Marina Berdnikova <berdni4ka@rambler.ru>
2023-02-12 17:50:54 +0300

    Add loader.py

--- study_project/src/loader.py
+++ study_project/src/loader.py
@@ -3,4 +3,8 @@ import requests
 
 def load_operations(url):
     response = requests.get(url)
+    #print(response.status_code)
+    #if response.status_code != 200:
+        #return None, f'ERROR: {response.status_code}'
+    #else:
     return response.json()
